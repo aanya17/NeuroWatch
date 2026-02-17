0e77aa0234b823ee548609a028282a8b16a34a8a
aanya17 <154660363+aanya17@users.noreply.github.com>
2026-02-17 21:16:46 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,3 +5,19 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Gait from "./pages/Gait";
 import VoiceAnalysis from "./pages/Voice";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/gait" element={<Gait />} />
+        <Route path="/voice" element={<VoiceAnalysis />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
