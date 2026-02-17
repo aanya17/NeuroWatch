a65fe01057b67513754f07e4a988bfc2caccfd4e
aanya17 <154660363+aanya17@users.noreply.github.com>
2026-02-17 19:33:49 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,23 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
-import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function App() {
+import { Login } from "./app/pages/Login";
+import { Signup } from "./app/pages/Signup";
+import { Dashboard } from "./app/pages/Dashboard";
+import { Gait } from "./app/pages/Gait";
+import { VoiceAnalysis } from "./app/pages/Voice";
+
+function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/gait" element={<Gait />} />
+        <Route path="/voice" element={<VoiceAnalysis />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
