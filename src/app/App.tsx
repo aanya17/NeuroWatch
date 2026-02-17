f5201670166c90f25e6876b889fefbf194677d76
aanya17 <154660363+aanya17@users.noreply.github.com>
2026-02-17 21:55:57 +0530

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Gait from "./pages/Gait";
+import VoiceAnalysis from "./pages/Voice";
+
 function App() {
   return (
     <Router>
